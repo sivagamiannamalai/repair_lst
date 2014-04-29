8afb16338fac60e9d333f7890b88d069b73939ab
Sivagami Annamalai <sannamalai@catalystitservices.com>
2014-04-29 14:36:36 -0700

    Added IsNotNull and IsValidLength subservices

---
+++ src/main/java/com/pdxcycle9/repair_lst/subservices/IsNotNull.java
@@ -0,0 +1,49 @@
+package com.pdxcycle9.repair_lst.subservices;
+
+import java.util.List;
+import com.pdxcycle9.repair_lst.util.Error;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class IsNotNull {
+	/**
+	 * This subservice checks if a null result is sent to a service.
+	 * This should only be possible to initiate through the Advanced REST client
+	 * @param string
+	 * @param errors
+	 * @return
+	 */
+	public boolean isFieldNotNull(String string, List<String> errors) {
+
+		boolean result = false;
+
+		if (string != null) {
+
+			return true;
+		} else {
+			errors.add(Error.IS_NULL);
+		}
+
+		return result;
+
+	}
+	
+	public boolean isFieldNotEmpty(double val, List<String> errors) {
+
+		boolean result = false;
+
+		if (val > 0) {
+
+			return true;
+		} else {
+			errors.add(Error.IS_NULL);
+		}
+
+		return result;
+
+	}
+
+
+}
