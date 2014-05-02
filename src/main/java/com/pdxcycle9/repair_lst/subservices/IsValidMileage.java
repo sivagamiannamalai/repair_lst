ce42a1c59668a76eb58d61049dbffcb772f5a075
Erwin Peil <epeil@catalystitservices.com>
2014-05-02 10:36:15 -0700

    Updated valid mileage subservice and error message for valid mileage (removed comma)

--- src/main/java/com/pdxcycle9/repair_lst/subservices/IsValidMileage.java
+++ src/main/java/com/pdxcycle9/repair_lst/subservices/IsValidMileage.java
@@ -1,5 +1,23 @@
 package com.pdxcycle9.repair_lst.subservices;
 
+import java.util.List;
+
+import com.pdxcycle9.repair_lst.util.Error;
+
 public class IsValidMileage {
+	
+	public boolean between1and999999(int miles, List<String> errors) {
+
+		boolean result = false;
+
+		if (miles >= 0 && miles <= 999999) {
+			result = true;
+		} else {
+			errors.add(Error.MILEAGE_IS_OUT_OF_RANGE);
+		}
+
+		return result;
+
+	}
 
 }
