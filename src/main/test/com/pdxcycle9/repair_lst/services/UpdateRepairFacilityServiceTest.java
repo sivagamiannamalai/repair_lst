a0540c5e57d9a90dd0eec13a1292dbf3bb896cbd
John Wanczyk <jwanczyk@catalystitservices.com>
2014-05-06 13:13:53 -0700

    unit test working for update repair facility

--- src/main/test/com/pdxcycle9/repair_lst/services/UpdateRepairFacilityServiceTest.java
+++ src/main/test/com/pdxcycle9/repair_lst/services/UpdateRepairFacilityServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,14 +29,18 @@ import com.pdxcycle9.repair_lst.util.Response;
 @RunWith(MockitoJUnitRunner.class)
 public class UpdateRepairFacilityServiceTest {
 	
-	static int[] specialization;
-	private static final int[] Collection = specialization;
-	RepairFacility repairFacility;
-	Response response;
-	Address address;
-	RepairFacility original;
-	RepairFacility updated;
-	
+	private int[] specializationArray;
+    private List<Specialization> specializationList;
+    private Response response;
+    private Address address;
+    private RepairFacility original;
+    private RepairFacility updated;
+    private String name;
+    private String phone;
+    private BigDecimal hourlyRate;
+    private Specialization specializationZero;
+    private Specialization specializationOne;
+    private Specialization specializationTwo;
 	
 	@Mock
 	private IsNotNull isNotNull;
@@ -52,57 +57,71 @@ public class UpdateRepairFacilityServiceTest {
 	@Before
 	public void setUp() throws Exception {
 		
-		repairFacility = new RepairFacility();
-		response = new Response();
-		specialization = new int[3];
-		specialization[0] = 1;
-		specialization[1] = 2;
-		specialization[2] = 3;
-		address = 1;
-		
-		String name = "Autozone";
-		String phone = "9876543210";
-		BigDecimal hourlyRate = new BigDecimal("20.00");
-		//specialization = new int[](specialization);
-		
-		repairFacility.setName(name);
-		repairFacility.setPhone(phone);
-		repairFacility.setHourlyRate(hourlyRate);
-		//repairFacility.setSpecializations(specialization);
-		repairFacility.setHourlyRate(hourlyRate);
-		repairFacility.setAddress(address);
-		//repairFacility.setSpecializations(Collection<specialization>);
-		
-		when(repairFacilityDAO.updateRepairFacility(repairFacility)).thenReturn(repairFacility);
-		when(updateRepairFacilityService.updateRepairFacility(original, updated).thenReuturn(repairFacility));
-		when(isValidLength.between1and255(Matchers.anyString(), Matchers.anyList())).thenReturn(true);
+		original = new RepairFacility();
+        updated = new RepairFacility();
+        specializationList = new ArrayList<Specialization>();
+
+        response = new Response();
+        specializationArray = new int[3];
+        specializationArray[0] = 1;
+        specializationArray[1] = 2;
+        specializationArray[2] = 3;
+        address = new Address(1);
+
+        specializationZero = new Specialization(1);
+        specializationOne = new Specialization(2);
+        specializationTwo = new Specialization(3);
+
+        specializationList.add(specializationZero);
+        specializationList.add(specializationOne);
+        specializationList.add(specializationTwo);
+
+        name = "Autozone";
+        phone = "9876543210";
+        hourlyRate = new BigDecimal("20.00");
+
+        updated.setName(name);
+        updated.setPhone(phone);
+        updated.setHourlyRate(hourlyRate);
+        updated.setAddress(address);
+        updated.setSpecializations(specializationList);
+
+        original.setName("New Name");
+        original.setPhone("0123456789");
+        original.setHourlyRate(new BigDecimal("100.00"));
+        original.setAddress(new Address(2));
+        original.setSpecializations(new ArrayList<Specialization>());
+
+		when(repairFacilityDAO.updateRepairFacility(original)).thenReturn(updated);
+		when(repairFacilityDAO.retrieveRepairFacilityByID(Matchers.anyInt())).thenReturn(original).thenReturn(updated);
+		when(isValidLength.between1and255(Matchers.anyString(),Matchers.anyList())).thenReturn(true);
 		when(isNotNull.isFieldNotNull(Matchers.anyString(), Matchers.anyList())).thenReturn(true);
-		when(isNotNull.isHourlyRateValid((BigDecimal)Matchers.any(), Matchers.anyList())).thenReturn(true);
-		when(isValidLength.isPhoneValidLength(Matchers.anyString(), Matchers.anyList())).thenReturn(true);
-		
+		when(isNotNull.isHourlyRateValid((BigDecimal) Matchers.any(),Matchers.anyList())).thenReturn(true);
+		when(isValidLength.isPhoneValidLength(Matchers.anyString(),Matchers.anyList())).thenReturn(true);
+
 	}
 	
-	@Test		
+	@Test
 	public void repairFacilityUpdatesCorrectly() throws Exception {
- 		response = updateRepairFacilityService.updateRepairFacility(repairFacility, specialization);
- 		assertEquals(HttpStatus.OK, response.getStatusCode());
+		response = updateRepairFacilityService.updateRepairFacility(updated,
+				specializationArray);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(RepairFacility.class, response.getResponseObject().getClass());
-		
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Test
 	public void giveNullforNameAndPhone() throws Exception {
-		String name = "";
-		String phone = "";
-		repairFacility.setName(name);
-		repairFacility.setPhone(phone);	
-				
+
+		updated.setName("");
+		updated.setPhone("");
+
 		when(isNotNull.isFieldNotNull(Matchers.anyString(), Matchers.anyList())).thenReturn(false);
-		when(isValidLength.isPhoneValidLength(Matchers.anyString(), Matchers.anyList())).thenReturn(false);
-		
-		response = updateRepairFacilityService.updateRepairFacility(repairFacility, specialization);
- 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		when(isValidLength.isPhoneValidLength(Matchers.anyString(),Matchers.anyList())).thenReturn(false);
+
+		response = updateRepairFacilityService.updateRepairFacility(updated, specializationArray);
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
 	}
 
 }
