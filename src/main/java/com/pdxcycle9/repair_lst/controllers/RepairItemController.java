53601a6765b1416178a71a2890e7ee4e77cfbe90
John Wanczyk <jwanczyk@catalystitservices.com>
2014-05-08 15:50:31 -0700

    commit to sync with parts entity

---
+++ src/main/java/com/pdxcycle9/repair_lst/controllers/RepairItemController.java
@@ -0,0 +1,61 @@
+package com.pdxcycle9.repair_lst.controllers;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pdxcycle9.repair_lst.entities.Address;
+import com.pdxcycle9.repair_lst.entities.RepairFacility;
+import com.pdxcycle9.repair_lst.entities.RepairItem;
+import com.pdxcycle9.repair_lst.services.CreateRepairFacilityService;
+import com.pdxcycle9.repair_lst.services.SearchRepairFacilityService;
+import com.pdxcycle9.repair_lst.services.UpdateRepairFacilityService;
+import com.pdxcycle9.repair_lst.util.Response;
+
+@Controller
+public class RepairItemController {
+	
+	@RequestMapping(value = "/repairitem", params = { "description", "date",
+			"hourlyRate", "hoursWorked", "mileage", "rating", "userId", "repairTypeId", 
+			"repairFacilityId", "vehicleId", "parts[]" }, method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public ResponseEntity<Object> createRepairFacility(
+			@RequestParam(value = "description") String description,
+			@RequestParam(value = "date") Date date,
+			@RequestParam(value = "hourlyRate") BigDecimal hourlyRate,
+			@RequestParam(value = "laborHours") int laborHours,
+			@RequestParam(value = "mileage") int mileage,
+			@RequestParam(value = "rating") int rating,
+			@RequestParam(value = "userId") int userId,
+			@RequestParam(value = "repairTypeId") int repairTypeId,
+			@RequestParam(value = "repairFacilityId") int repairFacilityId,
+			@RequestParam(value = "vehicleId") int vehicleId,
+			@RequestParam(value = "parts[]") int[] parts){
+
+		RepairItem repairItem = new RepairItem();
+
+		repairItem.setDescription(description);
+		repairItem.setDate(date);
+		repairItem.setHourlyRate(hourlyRate);
+		repairItem.setLaborHours(laborHours);
+		repairItem.setMileage(mileage);
+		repairItem.setRating(rating);
+		repairItem.setUserId(userId);
+		repairItem.setRepairTypeId(repairTypeId);
+		repairItem.setRepairFacilityId(repairFacilityId);
+		repairItem.setVehicleId(vehicleId);
+
+		Response response = createRepairItemService.createRepairItem(repairItem, parts);
+
+		return new ResponseEntity<Object>(response.getResponseObject(), response.getStatusCode());
+	}
+
+}
---
+++ src/main/java/com/pdxcycle9/repair_lst/entities/RepairItem.java
@@ -0,0 +1,191 @@
+package com.pdxcycle9.repair_lst.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+//@NamedQueries({ @NamedQuery(name = "findAllRepairItems", query = "SELECT ri FROM RepairItem ri"),
+//				@NamedQuery(name = "findRepairItemById", query = "SELECT ri FROM RepairItem ri WHERE ri.id = :id") })
+@Entity
+@Table(name = "repair_item")
+public class RepairItem implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "repair_item_id")
+	private int id;
+
+	@Column(name = "description", unique = true, nullable = true)
+	private String description;
+
+	@Column(name = "date", nullable = false)
+	private Date date;
+
+	@Column(name = "hourly_rate", nullable = false)
+	private BigDecimal hourlyRate;
+	
+	@Column(name = "labor_hours", nullable = false)
+	private int laborHours;
+	
+	@Column(name = "mileage", nullable = false)
+	private int mileage;
+	
+	@Column(name = "rating", nullable = false)
+	private int rating;
+
+	@Column(name = "user_fk", nullable = false)
+	private int userId;
+	
+	@Column(name = "repair_type_fk", nullable = false)
+	private int repairTypeId;
+	
+	@Column(name = "repair_facility_fk", nullable = false)
+	private int repairFacilityId;
+	
+	@Column(name = "vehicle_fk", nullable = false)
+	private int vehicleId;
+	
+	
+
+	@ManyToMany(fetch= FetchType.EAGER)
+	@JoinTable(name = "repair_item_parts", 
+	joinColumns = { @JoinColumn(name = "repair_item_fk") }, 
+	inverseJoinColumns = { @JoinColumn(name = "part_fk") })
+	private Collection<Part> parts;
+
+	public RepairItem() {
+
+	}
+	
+	/**
+	 * getters and setters for Repair Item
+	 * @return
+	 */
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public BigDecimal getHourlyRate() {
+		return hourlyRate;
+	}
+
+	public void setHourlyRate(BigDecimal hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+
+	public int getLaborHours() {
+		return laborHours;
+	}
+
+	public void setLaborHours(int laborHours) {
+		this.laborHours = laborHours;
+	}
+
+	public int getMileage() {
+		return mileage;
+	}
+
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getRepairTypeId() {
+		return repairTypeId;
+	}
+
+	public void setRepairTypeId(int repairTypeId) {
+		this.repairTypeId = repairTypeId;
+	}
+
+	public int getRepairFacilityId() {
+		return repairFacilityId;
+	}
+
+	public void setRepairFacilityId(int repairFacilityId) {
+		this.repairFacilityId = repairFacilityId;
+	}
+
+	public int getVehicleId() {
+		return vehicleId;
+	}
+
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public Collection<Part> getParts() {
+		return parts;
+	}
+
+	public void setParts(Collection<Part> parts) {
+		this.parts = parts;
+	}
+
+	@Override
+	public String toString() {
+		return "RepairItem [id=" + id + ", description=" + description + ", date="
+				+ date + ", hourlyRate=" + hourlyRate + ", laborHours=" + laborHours + ", mileage="
+				+ mileage + ", rating=" + rating + ", userId=" + userId + ", repairTypeId="
+				+ repairTypeId + ", repairFacilityId=" + repairFacilityId + ", vehicleId=" + vehicleId + ","
+				+ "parts=" + parts + "]";
+	}
+	
+	
+
+}
